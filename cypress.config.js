32e81cbf68c76ff0dcb9a8bdc5a14621d23a38db
Alberto Martínez <albertomz@gmail.com>
2025-08-15 14:04:58 +0200

    Agente [BACKEND]: Completa la construcción de la etapa

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
-    setupNodeEvents(on, config) {},
+    supportFile: false, // <-- LÍNEA CLAVE AÑADIDA
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
